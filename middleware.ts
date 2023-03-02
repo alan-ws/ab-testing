15913f61db894ff89bbe87d0aa11be52beee36d8
Alan Williams <alan.williams@vercel.com>
2023-03-02 13:49:55 +0000

    init middleware

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
+
+export function middleware(req: NextRequest) {
+  return NextResponse.rewrite(req.nextUrl);
+}
