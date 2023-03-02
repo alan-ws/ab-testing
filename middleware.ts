d3a18fc93ffb267ba5c66203bea29cc7918da2a3
Alan Williams <alan.williams@vercel.com>
2023-03-02 13:58:54 +0000

    middleware done

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,19 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { get } from "@vercel/edge-config";
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/", "/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
 
-export function middleware(req: NextRequest) {
-  return NextResponse.rewrite(req.nextUrl);
+const whichBucket = (): number => Math.round(Math.random());
+const buckets: { [key: number]: string } = {
+  0: "bucket-a",
+  1: "bucket-b",
+};
+
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  const url = await get(buckets[whichBucket()]);
+  return NextResponse.rewrite(new URL(pathname, url));
 }
