27311057e7e26e9d0dd80f1076d12632217035dc
Alan Williams <alan.williams@vercel.com>
2023-03-03 11:20:26 +0000

    test cache headers

--- pages/header/cache-control.tsx
+++ pages/header/cache-control.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <h1>we got the headers</h1>;
+export async function getServerSideProps() {
+  return {
+    props: {
+      data: 52,
+    }, // will be passed to the page component as props
+  };
+}
+export default function Home({ data }: any) {
+  return <h1>we got the headers: {data}</h1>;
 }
